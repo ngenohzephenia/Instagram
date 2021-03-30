4d78977252d0fdbf6ff68817ef31cb05b529c3b6
ngenohzephenia <ngenohzef@gmail.com>
2021-03-30 17:13:04 +0300

    created classes in view.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from posts.models import Post
+from users.models import Profile
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post Admin"""
+    list_display=('__str__', 'title', 'photo','created','modified')
+    readonly_fields=('created', 'modified')
+    list_editable=('title','photo')
+    search_fields=('profile__user__email','profile__user__username','profile__user__first_name','profile__user__last_name','title')
+    list_filter=('profile__user__is_active','profile__user__is_staff','created','modified',)
