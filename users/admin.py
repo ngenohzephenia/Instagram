89eb12b0d06f27f75e620c3f9288cc2ad6f1d628
ngenohzephenia <ngenohzef@gmail.com>
2021-03-31 15:27:53 +0300

    update users admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from users.models import Profile
 # Register your models here.
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """Profile Admin"""
+list_display=('pk','user','phone_number','website','picture')
+list_display_links=('pk','user')
+list_editable=('phone_number','website','picture')
+search_fields=('user__email','user__username','user__first_name','user__last_name','phone_number')
+list_filter=('user__is_active','user__is_staff','created','modified',)
+fieldsets=(('profile',{'fields':(('user','picture'),)}),('Extra info',{'fields':(('phone_number','website'),('biography'),)}),('metadata',{'fields':(('created','modified'),)}),)
+readonly_fields =('created','modified')
+
+
+class ProfileInline(admin.StackedInline):
+        """Profile in-line admin for users."""
+        model = Profile
+        can_delete = False
+        verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin."""
+    inlines = (ProfileInline, )
+    list_display = ( 'username','first_name', 'last_name','is_active', 'is_staff',)
+
+
+admin.site.unregister(User)
+# admin.site.register(Modelo,Clase)
+admin.site.register(User, UserAdmin)
